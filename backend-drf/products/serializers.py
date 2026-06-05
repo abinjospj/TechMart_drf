8990006a5beb2ada659469fafa38debe4f6b4872
abinjospj <abinjospj0722@gmail.com>
2026-06-06 01:13:45 +0530

    Fix media image URL in serializer

--- backend-drf/products/serializers.py
+++ backend-drf/products/serializers.py
@@ -2,6 +2,10 @@ from rest_framework import serializers
 from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
     class Meta:
         model = Product
         fields = "__all__"
+
+    def get_image(self, obj):
+        return obj.image.url if obj.image else None    
